c7b06cfbb695fbeafb58a8d8e040359b240bfd56
Hamad Khalil <123974803+Hamad-Khalil@users.noreply.github.com>
2023-03-01 00:01:07 +0500

    Add files via upload

---
+++ Assignment 9/Assignment 9 (2)/Question16.js
@@ -0,0 +1,3 @@
+let array=[1,2,3,4,5,6,7]
+let newArray= array.reverse()
+console.log(newArray);
---
+++ Assignment 9/Assignment 9 (2)/Question2.js
@@ -0,0 +1,19 @@
+let myArray=[1,-2,3,-4,5,-6,7,-8]
+function Negative(array){
+let Negative= []
+for( i=0; i<= array.length; i++){
+ if( array[i] <0){
+    Negative.push( array[i])
+ }
+}
+ return Negative
+}
+console.log(Negative(myArray));
+
+// for(i=0; i<= array.length; i++){
+//     if( array < 0){
+//         let Negative = array<0
+//     }
+//     console.log('positive');
+// }
+
